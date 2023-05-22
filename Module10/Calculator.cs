bb8f8ba71e2434d2d415f1bb5ec06074eaf9d131
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-05-22 21:03:22 +0300

    Итоговое задание модуля 10

---
+++ Module10/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    public class Calculator : ICalculator
+    {
+        ILogger Logger { get; }
+
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        void ICalculator.Calc(int x, int y)
+        {
+            Logger.Event("Начинаю подсчет...");
+            Console.WriteLine($"Сумма введенных чисел: {x + y}\n\n");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Module10/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Module10
+{
+
+
+
+    public class Program
+    {
+        static ILogger logger { get; set; }
+        static void Main(string[] args)
+        {
+            logger = new Logger();
+            Calculator calculator = new Calculator(logger);
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Калькулятор сложения двух чисел.\n\nВведите два числа:\n");
+                    Console.Write("Первое число: ");
+                    int number1 = int.Parse(Console.ReadLine());
+                    Console.Write("Второе число: ");
+                    int number2 = int.Parse(Console.ReadLine());
+                    ((ICalculator)calculator).Calc(number1, number2);
+
+                }
+                catch (FormatException)
+                {
+                    logger.Error("Произошла ошибка...");
+                    Console.WriteLine("\nВведено некорректное значение\n");
+                    
+                }
+            }
+
+
+        }
+
+    }
+
+
+    public interface ICalculator
+    {
+
+        public void Calc(int x, int y);
+
+    }
+
+    public interface ILogger
+    {
+        void Error(string message);
+        void Event(string message);
+    
+    }
+
+
+
+
+
+
+
+}
